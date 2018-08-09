7b33aeb96e5963562da615275c12a2db634318c4
Seiei Miyagi <hanachin@gmail.com>
2018-08-09 13:09:35 +0900

    Add ESP::BLE::GAP::ScanStartCmpl#bda, #search_evt

--- src/mrb_esp_gap_cb_param.c
+++ src/mrb_esp_gap_cb_param.c
@@ -19,11 +19,28 @@ static mrb_value mrb_esp32_scan_start_cmpl_status(mrb_state *mrb, mrb_value self
   return mrb_fixnum_value(param->scan_start_cmpl.status);
 }
 
+static mrb_value mrb_esp32_scan_rst_bda(mrb_state *mrb, mrb_value self) {
+  esp_ble_gap_cb_param_t *param = (esp_ble_gap_cb_param_t *)mrb_data_get_ptr(mrb, self, &mrb_esp32_ble_gap_cb_param_type);
+  uint8_t *bda = param->scan_rst.bda;
+  return mrb_str_new_static(mrb, (char *)bda, ESP_BD_ADDR_LEN);
+}
+
+static mrb_value mrb_esp32_scan_rst_search_evt(mrb_state *mrb, mrb_value self) {
+  esp_ble_gap_cb_param_t *param = (esp_ble_gap_cb_param_t *)mrb_data_get_ptr(mrb, self, &mrb_esp32_ble_gap_cb_param_type);
+  return mrb_fixnum_value(param->scan_rst.search_evt);
+}
+
 static void scan_start_cmpl_init(mrb_state *mrb, struct RClass *scan_start_cmpl) {
   mrb_define_method(mrb, scan_start_cmpl, "success?", mrb_esp32_scan_start_cmpl_success_p, MRB_ARGS_NONE());
   mrb_define_method(mrb, scan_start_cmpl, "status", mrb_esp32_scan_start_cmpl_status, MRB_ARGS_NONE());
 }
 
+static void scan_rst_init(mrb_state *mrb, struct RClass *scan_rst) {
+  mrb_define_method(mrb, scan_rst, "bda", mrb_esp32_scan_rst_bda, MRB_ARGS_NONE());
+  mrb_define_method(mrb, scan_rst, "search_evt", mrb_esp32_scan_rst_search_evt, MRB_ARGS_NONE());
+}
+
 void mrb_esp_gap_cb_param_init(mrb_state *mrb, struct RClass *mrb_esp32_ble_gap) {
   scan_start_cmpl_init(mrb, mrb_class_get_under(mrb, mrb_esp32_ble_gap, "ScanStartCmpl"));
+  scan_rst_init(mrb, mrb_class_get_under(mrb, mrb_esp32_ble_gap, "ScanRst"));
 }
